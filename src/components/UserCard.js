2580530e4715f2b0a21b07f8bde56e33f362f425
Mark Galang <galangmarkg@gmail.com>
2020-12-23 17:27:39 -0500

    Initial Card Styling

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
 
 const UserCard = (props) => {
   const { name, company, isFave, imageURL } = props.userInfo;
   return (
     <View style={styles.userCard}>
-      <View>
-        <Image source={{ uri: imageURL }} style={{ width: 100, height: 100 }} />
+      <View
+        style={[styles.profilePictureContainer, styles.parallelogramContainer]}>
+        <ImageBackground
+          source={{ uri: imageURL }}
+          style={styles.profilePicture}
+        />
       </View>
-      <View>
-        <Text>{name}</Text>
-        <Text>{company.name}</Text>
-        <TouchableOpacity>
-          <Text>{isFave ? 'FAVED' : 'fave'}</Text>
-        </TouchableOpacity>
+      <View style={[styles.userInfoContainer, styles.parallelogramContainer]}>
+        <View style={styles.userInfoContainer}>
+          <Text>{name}</Text>
+          <Text>{company.name}</Text>
+          <TouchableOpacity>
+            <Text>{isFave ? 'FAVED' : 'fave'}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -22,11 +34,34 @@ const UserCard = (props) => {
 const styles = StyleSheet.create({
   userCard: {
     borderColor: 'red',
-    borderWidth: 1,
-    padding: 5,
+    borderWidth: 2,
     marginBottom: 5,
     backgroundColor: '#d6ffd8',
     flexDirection: 'row',
+  },
+  parallelogramContainer: {
+    transform: [{ skewX: '-10deg' }],
+    left: -20,
+  },
+  profilePictureContainer: {
+    overflow: 'hidden',
+    backgroundColor: 'red',
+    height: 150,
+    width: 160,
+  },
+  profilePicture: {
+    // transform: [{ skewX: '10deg' }],
+    // flex: 1,
+    // width: '100%',
+    // justifyContent: 'flex-end',
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch'
+  },
+  userInfoContainer: {
+    borderColor: 'red',
+    borderWidth: 2,
+    // transform: [{ skewX: '10deg' }],
+    flex: 1,
   },
 });
 
