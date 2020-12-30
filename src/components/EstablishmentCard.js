a03f9cdb1e0dcec472cc1d446834f6cb525d6db1
Mark Galang <galangmarkg@gmail.com>
2020-12-30 16:40:09 -0500

    Create EstablishmentCard.js

---
+++ src/components/EstablishmentCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const EstablishmentCard = () => {
+  return (
+    <View>
+      <Text>ESTABLISHMENT CARD</Text>
+      <Text>Rating</Text>
+      <Text>Address</Text>
+      <Text>Open Now</Text>
+    </View>
+  );
+};
+
+export default EstablishmentCard;
