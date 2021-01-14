52548548648f2895da4ee5533961740b576263ae
Mark Galang <galangmarkg@gmail.com>
2021-01-14 16:55:02 -0500

    Add styling

--- src/screens/UserProfileScreen.js
+++ src/screens/UserProfileScreen.js
@@ -1,14 +1,121 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import { COLORS, HEADING } from '../styles/theme';
+
+const screenWidth = Dimensions.get('window').width;
 
 const UserProfileScreen = ({ route }) => {
-  const { name } = route.params.userInfo;
+  const { name, imageURL, company, isFave } = route.params.userInfo;
 
+  console.log(route.params.userInfo);
   return (
-    <View>
-      <Text>{name}</Text>
+    <View style={styles.userCard}>
+      <ImageBackground
+        style={{
+          width: screenWidth,
+          flexDirection: 'row',
+          backgroundColor: 'rgba(0,0,0,1)',
+          width: screenWidth,
+        }}
+        imageStyle={{
+          resizeMode: 'cover',
+          opacity: 0.6,
+        }}
+        source={{
+          uri: company.imageUrl,
+        }}>
+        <View style={styles.profilePictureContainer}>
+          <ImageBackground
+            source={{ uri: imageURL }}
+            style={styles.profilePicture}></ImageBackground>
+        </View>
+        <View style={styles.userInfoContainer}>
+          <Text style={[HEADING.heading2, styles.name, styles.textWhite]}>
+            {name}
+          </Text>
+          <Text style={[styles.location, styles.textWhite]}>
+            {company.name}
+          </Text>
+          <TouchableOpacity
+            onPress={() => console.log('FAVE BUTTON WAS PRESSED')}
+            style={[isFave ? styles.fave : styles.notFave, styles.button]}>
+            <Text style={{ color: COLORS.white }}>
+              {isFave ? 'FAVED' : 'FAVE'}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+      <View style={styles.dateContainer}>
+        <Text style={[styles.textWhite, { fontSize: 18, fontWeight: '700' }]}>
+          SCHED HERE
+        </Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  profilePictureContainer: {
+    transform: [{ skewX: '-5deg' }],
+    left: -20,
+    minHeight: 200,
+    height: '100%',
+    width: 180,
+    overflow: 'hidden',
+  },
+  profilePicture: {
+    flex: 1,
+    transform: [{ skewX: '5deg' }],
+    justifyContent: 'flex-end',
+    resizeMode: 'cover',
+    aspectRatio: 1.15,
+  },
+  userInfoContainer: {
+    overflow: 'hidden',
+    flex: 1,
+    width: screenWidth,
+    justifyContent: 'flex-end',
+  },
+  button: {
+    alignSelf: 'flex-start',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderRadius: 100,
+    borderColor: COLORS.white,
+    borderWidth: 2,
+    marginVertical: 15,
+  },
+  name: {
+    letterSpacing: 1,
+    marginBottom: 4,
+  },
+  location: {
+    fontSize: 18,
+  },
+  fave: {
+    backgroundColor: COLORS.primary,
+  },
+  notFave: {
+    backgroundColor: 'rgb(184, 184, 184)',
+  },
+  textWhite: {
+    color: COLORS.white,
+  },
+
+  dateContainer: {
+    backgroundColor: COLORS.secondary,
+    width: screenWidth,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+  },
+});
 
 export default UserProfileScreen;
